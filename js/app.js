31b053caa782935c7d1fed6e4def06bcf81c2a65
Joacir Gomes de Souza Filho <131211813+JoacirFilho@users.noreply.github.com>
2023-12-05 19:57:24 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,46 @@
+const divImagemPrincipal = document.querySelector("#imagem-principal");
+const imagemPrincipal = divImagemPrincipal.querySelector("img");
+const textoAlternativo = divImagemPrincipal.querySelector("#texto-alternativo");
+const btnProximo = divImagemPrincipal.querySelector(".proximo");
+const btnAnterior = divImagemPrincipal.querySelector(".anterior");
+const todasImagens = document.querySelectorAll("#imagens img");
+
+let idImagemAtiva = 0;
+
+const proximaImagem = function () {
+    idImagemAtiva++;
+    if (idImagemAtiva >= todasImagens.length) idImagemAtiva = 0;
+    selecionarImagem();
+};
+
+const voltarImagem = function () {
+    idImagemAtiva--;
+    if (idImagemAtiva < 0) idImagemAtiva = todasImagens.length - 1;
+    selecionarImagem();
+};
+
+const selecionarImagem = function () {
+    const imagemAtual = todasImagens[idImagemAtiva];
+
+    imagemPrincipal.src = imagemAtual.src;
+    textoAlternativo.textContent = imagemAtual.alt;
+
+    todasImagens.forEach(function (imagem, numeroImage) {
+        if (numeroImage === idImagemAtiva) {
+            imagem.classList.add("selecionada");
+        } else {
+            imagem.classList.remove("selecionada");
+        }
+    });
+};
+
+
+btnProximo.addEventListener("click", proximaImagem);
+btnAnterior.addEventListener("click", voltarImagem);
+
+todasImagens.forEach(function (imagem, numeroImage) {
+    imagem.addEventListener("click", function () {
+        idImagemAtiva = numeroImage;
+        selecionarImagem();
+    });
+});
